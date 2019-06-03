868505431ab2274be5c64b1c2915fcbcafc410b1
Bruno Coelho <brunocsncoelho@gmail.com>
2019-06-03 21:24:22 +0100

    code cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root to: "alerts#new"
   resources :alerts, only: [:create, :destroy, :index]
-  #resources :products, only: [:index]
-
-  # root to: "home#index"
-  # post "/create", to: "home#create"
-
-  # resources :users, param: :email, only: [] do
-  #   resources :products
-  # end
 end
